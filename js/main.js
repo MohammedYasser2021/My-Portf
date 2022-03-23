f0a9b1c205cce3172bfcbbc92272d8a33f393c38
MohammedYasser2021 <mohammedyasser19913@gmail.com>
2022-03-23 21:24:26 +0200

    edited

--- js/main.js
+++ js/main.js
@@ -60,18 +60,20 @@ document.querySelector('form').addEventListener('submit', (e) => {
 // open navbar
 let navOpen = document.querySelector('.navbar-open')
 let navbarMenu = document.querySelector('.navbar-menu')
+let navOpenIcon = document.querySelector('.bar')
 window.addEventListener('scroll', function () {
   if (this.scrollY > 80) {
     navOpen.style.right = '20px'
     navbarMenu.classList.remove('visible')
     navbarMenu.classList.add('hidden')
+    navOpenIcon.classList.remove('fa-xmark')
+    navOpenIcon.classList.add('fa-bars')
   } else {
     navOpen.style.right = '-60px'
     navbarMenu.classList.remove('hidden')
     navbarMenu.classList.add('visible')
   }
 })
-let navOpenIcon = document.querySelector('.bar')
 navOpen.addEventListener('click', function () {
   navbarMenu.classList.toggle('visible')
   if (navbarMenu.classList.contains('visible')) {
